41d1769ffb714d7aabbdb50cfa806d06031bd367
paklic9 <vojta.zamecnik@seznam.cz>
2020-05-10 22:16:09 +0200

    pwa to apk + fixed typo

--- src/pages/Lists.tsx
+++ src/pages/Lists.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {Button, Col, Form, FormControl, ListGroup, Modal, Row, Tab, Table} from "react-bootstrap";
-import {handleInputChange} from "../untils";
+import {handleInputChange} from "../utils";
 import useLists from "../hooks/useLists";
 import {IEditItem, IList, IListDetail, IState} from "../types";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
